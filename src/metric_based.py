b6e8172289de977d53e2e640e6142bc5809cf383
Charlene Leong <chaleong@deloitte.co.nz>
2019-07-30 17:21:59 +1200

    [docs] Added slides

---
+++ src/metric_based.py
@@ -0,0 +1,12 @@
+
+
+### Load features from fc7 train and test - obtain from finetune
+
+### Calculate average fc7 feature map for each class
+
+### Calculate cosine similarrity between test features and average fc7
+
+### Return the class with the highest  cosine similarity score
+
+
+
